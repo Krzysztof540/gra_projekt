d98ff4c3ed1b8440539bb405d5594f32c755765b
seba3334 <132669164+seba3334@users.noreply.github.com>
2023-05-07 17:47:46 +0200

    Update Kółko i krzyżyk.py

--- Kółko i krzyżyk.py
+++ Kółko i krzyżyk.py
@@ -6,7 +6,7 @@ for i in range(9):
     tabela.append(' ')
 
 
-def plansza (tabela):
+def plansza(tabela):
     board = f"""
    {tabela[0]} | {tabela[1]} | {tabela[2]}       1 | 2 | 3  
   ---|---|---     ---|---|---
@@ -17,24 +17,57 @@ def plansza (tabela):
     print(board)
 
 
-index_list = []
+wybraneLiczby = []
 
 
 def ruch(numerPola):
-    while True:
-        numerPola -= 1
-        if 0 <= numerPola <= 8:
-            if numerPola in index_list:
-                print(f'Na tym miejscu już stoi {tabela[numerPola]}')
-                continue
-            index_list.append(numerPola)
-            return numerPola
-        print('Podano liczbę z poza zakresu')
+    try:
+        numerPola = int(numerPola)
+    except:
+        return "Nie podano liczby"
+    numerPola = int(numerPola)
+    numerPola -= 1
+    print(numerPola)
+    if 0 <= numerPola <= 8:
+        if numerPola in wybraneLiczby:
+            return f'Na tym miejscu już stoi {tabela[numerPola]}'
+
+        wybraneLiczby.append(numerPola)
+        return numerPola
+    return 'Podano liczbę z poza zakresu'
+
 
 def ruchGracza():
-    x = int(input("Podaj liczbę z zakresu od 1 do 9:"))
-    ruch(x)
+    while True:
+        x = input("Podaj liczbę z zakresu od 1 do 9:")
+        y = ruch(x)
+        if type(y) != int:
+            print(y)
+            continue
+        if 0 <= y <= 8:
+            return y
+
 
 def ruchKomputera():
-    x = random.randint(0,9)
-    ruch(x)
+    while True:
+        x = random.randint(1, 9)
+        y = ruch(x)
+        if type(y) != int:
+            continue
+        if 0 <= y <= 8:
+            return y
+
+
+def result_calculation(tabela):
+    if tabela[0] == tabela[1] == tabela[2] == 'X' or tabela[1] == tabela[4] == tabela[7] == 'X' or tabela[0] == \
+            tabela[4] == tabela[8] == 'X' or tabela[2] == tabela[5] == tabela[8] == 'X' or tabela[3] == tabela[4] == \
+            tabela[5] == 'X' or tabela[2] == tabela[4] == tabela[6] == 'X' or tabela[6] == tabela[7] == tabela[8] == \
+            'X' or tabela[0] == tabela[3] == tabela[6] == 'X':
+        quit('Gracz1 (X) wygrał')
+    elif tabela[0] == tabela[1] == tabela[2] == 'O' or tabela[1] == tabela[4] == tabela[7] == 'O' or tabela[0] == \
+            tabela[4] == tabela[8] == 'O' or tabela[2] == tabela[5] == tabela[8] == 'O' or tabela[3] == tabela[4] == \
+            tabela[5] == 'O' or tabela[2] == tabela[4] == tabela[6] == 'O' or tabela[6] == tabela[7] == tabela[8] == \
+            'O' or tabela[0] == tabela[3] == tabela[6] == 'O':
+        quit("Gracz2 (y) wygrał")
+    else:
+        return
